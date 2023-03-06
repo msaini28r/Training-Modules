8e0fffaf9848f87604e78ebd91db71acb93315b7
msaini0r <msaini0r@gmail.com>
2023-03-06 23:49:47 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `myproject`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
